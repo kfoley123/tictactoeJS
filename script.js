9a6d30bfd11a9cfbdccb60ca243716302d8becb4
Kortney <kortney@iMac.local>
2022-03-04 21:33:59 -0400

    Added a class to each button. Stored each button in a variable in a button and gave them each a click eventlistener

--- script.js
+++ script.js
@@ -2,3 +2,25 @@
 //needs to have a gameboard/3x3 grid
 //when selected empty space takes either x or o
 //when three in a row x or o wins
+
+const buttonOne = document.querySelector(".buttonOne");
+const buttonTwo = document.querySelector(".buttonTwo");
+const buttonThree = document.querySelector(".buttonThree");
+
+const buttonFour = document.querySelector(".buttonFour");
+const buttonFive = document.querySelector(".buttonFive");
+const buttonSix = document.querySelector(".buttonSix");
+
+const buttonSeven = document.querySelector(".buttonSeven");
+const buttonEight = document.querySelector(".buttonEight");
+const buttonNine = document.querySelector(".buttonNine");
+
+buttonOne.addEventListener("click", (e) => {});
+buttonTwo.addEventListener("click", (e) => {});
+buttonThree.addEventListener("click", (e) => {});
+buttonFour.addEventListener("click", (e) => {});
+buttonFive.addEventListener("click", (e) => {});
+buttonSix.addEventListener("click", (e) => {});
+buttonSeven.addEventListener("click", (e) => {});
+buttonEight.addEventListener("click", (e) => {});
+buttonNine.addEventListener("click", (e) => {});
