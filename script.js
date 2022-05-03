0aa16a49c92b468f3446c168f66fa6fd09941e4c
Kortney <kortney@iMac.local>
2022-05-02 21:33:42 -0300

    added win scenario

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // board
 //player turns X & O
 //win scenarios
-//trie scenario
 
 let playerTurnX = true;
 
@@ -15,8 +14,19 @@ function placeMarker(e) {
     if (playerTurnX == true) {
         e.target.innerHTML = "X";
         playerTurnX = false;
+        checkForWin(buttons);
     } else {
         e.target.innerHTML = "O";
         playerTurnX = true;
+        checkForWin(buttons);
+    }
+}
+
+function checkForWin(buttonsArray) {
+    if (
+        buttonsArray[0].innerHTML == buttonsArray[1].innerHTML &&
+        buttonsArray[2].innerHTML == buttonsArray[0].innerHTML
+    ) {
+        alert("WIN");
     }
 }
